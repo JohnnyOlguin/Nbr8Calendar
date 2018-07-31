de9026f4b71a1d1c45c370a1f14ed28fda6f8e1a
Jonny Olguin <johnny_olguin@hotmail.com>
2018-07-31 19:22:46 -0400

    req. 3

--- calendar.js
+++ calendar.js
@@ -1,11 +1,26 @@
 function renderCalendar() {
-    var el = document.getElementById('month')
-    el.style.visibility = 'visible';
-    el.classList.remove('page-load');
-
-    printDayOfMonth();
+   createDays();
+   printDayOfMonth(5, 20);
 }
 
-function printDayOfMonth() {
-    
+function createDays() {
+    var day = 1;
+    for(var week = 1; week <= 4; week++) {
+        for(; day <= week * 7; day++) {
+            var daySquare = document.createElement('div');
+            daySquare.className = (day % 7 == 0 || (day-1) % 7 == 0) ? 'square weekend-day' : 'square';
+            document.getElementById('week' + week).appendChild(daySquare);
+
+            var dayContent = document.createElement('div');
+            dayContent.id = 'pos' + day;
+            dayContent.className = 'content';
+            daySquare.appendChild(dayContent);
+        }
+    }
+}
+
+function printDayOfMonth(initialDate, endDate) {
+    for (var i = initialDate; i <= endDate; i++) {
+        document.getElementById('pos' + i).innerHTML = i;
+    }
 }
